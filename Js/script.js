0b8a085c8a87ad07a442026a72c8f53071eb45ed
Phongtawat <mr.phongtawat@gmail.com>
2023-07-11 15:18:52 +0700

    setup

--- Js/script.js
+++ Js/script.js
@@ -1,1 +1,25 @@
-document.getElementById('title').innerText='บทที่1.1 อัพขึ้นgitครั้งละ20บาท เพื่อสมทบทุนเป็นกำลังใจในการเรียน 555'
+// let x = 11
+// let y = 22
+// let z = x+y
+
+// console.log(z)
+
+//     x = 15
+//     y = 6
+//     z =x*y
+
+// console.log(z)
+
+// // const birthYear = 1997 ประกาสตัวแปรที่ไม่เปลี่ยนแปลงใช้คำสั่ง const
+
+// console.log (birthYear)
+// //   birthYear =2000
+
+
+// // แบบที่1
+// let salary = 18000
+// console.log (salary*12)
+// แบบที่2
+let salary = 18000
+let yearlyIncome =salary*12
+console.log (yearlyIncome)
